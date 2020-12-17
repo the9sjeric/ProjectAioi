b377e91c3bd8ed349f282a5d9e7e6fd1ee89ec3e
the9sjeric <the9sjeric@hotmail.com>
2020-12-17 17:17:48 +0800

    上传练习

--- Practice/for&list.py
+++ Practice/for&list.py
@@ -1,4 +1,4 @@
-pizza =  ['zhixin','haixian','niuyou']
+pizza = ['zhixin', 'haixian', 'niuyou']
 
 for pisa in pizza:
     print(f'我喜欢吃{pisa.title()}披萨。')
@@ -7,3 +7,21 @@ print(f'{pizza[0]}披萨很软')
 print(f'{pizza[1]}披萨很鲜')
 print(f'{pizza[2]}披萨肉很多')
 print(f'我太爱吃披萨了。我老婆最爱之{pizza[0]}')
+
+animal = ['dog', 'cat', 'mouse']
+
+for pet in animal:
+    a = animal[0]
+    b = animal[1]
+    c = animal[2]
+    if pet == a:
+        print(f'{pet.title()}听话，不过比较吵。')
+    else:
+        pass
+    if pet == b:
+        print(f'{pet.title()}非常安静，喜欢独立')
+    else:
+        pass
+    if pet == c:
+        print(f'{pet.title()}吃得少，不过寿命短')
+print('只要你喜欢，他们都是好宠物！')
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# 这是一个示例 Python 脚本。
+pizza =  ['zhixin','haixian','niuyou']
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+for pisa in pizza:
+    print(f'我喜欢吃{pisa.title()}披萨。')
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+print(f'{pizza[0]}披萨很软')
+print(f'{pizza[1]}披萨很鲜')
+print(f'{pizza[2]}披萨肉很多')
+print(f'我太爱吃披萨了。我老婆最爱之{pizza[0]}')
