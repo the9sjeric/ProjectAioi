b701ba4ca22948002969a5a7411b2beb5e2071d6
the9sjeric <the9sjeric@hotmail.com>
2020-12-17 17:31:25 +0800

    上传练习

--- Practice/int.py
+++ Practice/int.py
@@ -1,2 +1,18 @@
-for value in range(1,5):
+for value in range(1, 5):
     print(value)
+
+shuzi = list(range(2, 7, 2))
+print(shuzi)
+
+pingfang = []
+for i in range(1, 11):
+    # aaa = i ** 2
+    pingfang.append(i**2)
+print(pingfang)
+
+print(min(pingfang))
+print(max(pingfang))
+print(sum(pingfang))
+
+xin = [b**2 for b in range(1,6)]
+print(xin)
