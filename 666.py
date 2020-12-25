87fc7bcc74b27b6ba443778bfac9cf5008d34e08
the9sjeric <the9sjeric@hotmail.com>
2020-12-25 17:30:26 +0800

    上传练习20201223

--- 666.py
+++ 666.py
@@ -1,7 +1,9 @@
-g = [0, 2, 2, 3]
-s = [0, 1, 2, 2]
-s = sorted(s)
+g = [2, 1, 2, 3]
+s = [2, 1, 1, 2]
 g = sorted(g)
+print(g)
+s = sorted(s)
+print(s)
 ss = len(s) - 1
 j = 0
 for i in range(0, ss):
